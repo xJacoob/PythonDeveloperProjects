8c935a01890f12c41a4debd2c55328e81afc6d74
Kuba.K <kuba.kosowicz12@gmail.com>
2026-07-13 20:22:58 +0200

    LuhnAlgorithm

--- Simple Banking System (Python)/task/banking/card.py
+++ Simple Banking System (Python)/task/banking/card.py
@@ -5,11 +5,25 @@ class Card:
     card_prefix = '400000'
     
     def __init__(self, account_number):
-        self.checksum = random.randint(0, 9)
+        self.account_number = account_number
+        self.checksum = Card.luhn_algorithm(self.card_prefix + self.account_number)
         self.card_number = self.card_prefix + account_number + str(self.checksum)
         self.pin = ''.join(str(random.randint(0, 9)) for _ in range(4))
-        self.account_number = account_number
         self.balance = 0
+
+    @staticmethod
+    def luhn_algorithm(digits: str) -> int:
+        number_list = [int(x) for x in digits]
+        for i in range(len(number_list)):
+            if (i + 1) % 2 != 0:
+                number_list[i] = int(number_list[i]) * 2
+
+            if number_list[i] > 9:
+                number_list[i] -= 9
+
+        sum_number = sum(number_list)
+        check_sum = (10 - (sum_number % 10)) % 10
+        return check_sum
 
     def __str__(self):
         return f"Your card number:\n{self.card_number}\nYour card PIN:\n{self.pin}"
