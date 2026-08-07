955451e5219946476b2801167bc233546650469a
xJacoob <kuba.kosowicz11@gmail.com>
2026-08-07 22:15:18 +0200

    Update hack.py

--- Password Hacker with Python/task/hacking/hack.py
+++ Password Hacker with Python/task/hacking/hack.py
@@ -1,38 +1,33 @@
 import socket
 import sys
+import itertools
+import string
 
 def main():
-    if len(sys.argv) < 4:
+    if len(sys.argv) < 3:
         print("Error! Correct usage: 'script name' 'localhost' 'port' 'message you want to send' ")
         sys.exit()
 
     host = sys.argv[1]
     port = int(sys.argv[2])
-    message = sys.argv[3]
 
-    connect_with_server(host, port, message)
+    find_password(host, port)
 
-def connect_with_server(host, port, message):
-    # Creating the socket
+def find_password(host, port):
+    all_characters = string.ascii_lowercase + string.digits
     client_socket = socket.socket()
-
-    # Connecting to the server
     client_socket.connect((host, port))
 
-    # Converting data to bytes
-    message = message.encode()
-
-    # Sending through socket
-    client_socket.send(message)
-
-    # Receiving the response
-    response = client_socket.recv(1024)
-
-    # Decoding from bytes to string
-    response = response.decode()
-    print(response)
-
-    client_socket.close()
+    for length in itertools.count(1):
+        for p in itertools.product(all_characters, repeat=length):
+            guess = "".join(p)
+            client_socket.send(guess.encode())
+            response = client_socket.recv(1024)
+            response = response.decode()
+            if response == "Connection success!":
+                print(guess)
+                client_socket.close()
+                return
 
 if __name__ == "__main__":
     main()
