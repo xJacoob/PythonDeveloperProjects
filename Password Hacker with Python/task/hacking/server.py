dfcf25360a9b4f86c7fc51e95e006852ba447b76
Jakub Kosowicz <kuba.kosowicz11@gmail.com>
2026-08-09 20:57:54 +0200

    Update server.py

--- Password Hacker with Python/task/hacking/server.py
+++ Password Hacker with Python/task/hacking/server.py
@@ -1,1 +1,46 @@
+import socket
+import json
 
+host = 'localhost'
+port = 9090
+
+login = 'user2'
+password = '12345'
+
+print(f"[TEST SERVER] login={login}, password={password}")
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen(1)
+print(f"LISTENING on {host}:{port}")
+
+conn, addr = server.accept()
+print("Client connected")
+
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+
+    try:
+        request = json.loads(data.decode())
+        req_login = request["login"]
+        req_password = request["password"]
+    except (json.JSONDecodeError, KeyError):
+        response = {'result': 'Bad request!'}
+        conn.sendall(json.dumps(response).encode())
+        continue
+
+    if req_login != login:
+        response = {'result': 'Wrong login!'}
+    elif req_password == password:
+        response = {'result': 'Connection success!'}
+    elif password.startswith(req_password):
+        response = {'result': 'Exception happened during login'}
+    else:
+        response = {'result': 'Wrong password!'}
+
+    conn.sendall(json.dumps(response).encode())
+
+conn.close()
+server.close()
