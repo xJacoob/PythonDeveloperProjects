38cb756493cb706f6925aa881d297e10a4b55e1f
Jakub Kosowicz <kuba.kosowicz11@gmail.com>
2026-08-26 22:10:26 +0200

    Update translator_class.py

--- Multilingual Online Translator/translator_class.py
+++ Multilingual Online Translator/translator_class.py
@@ -35,23 +35,52 @@ class Translator:
 
         words = [word.text for word in html_words]
 
-        examples_container = soup.find('div', class_='example_lines inexact')
         examples = []
 
+        context_line = valid_class.find_all('span', class_='tag_e')
+
+        for line in context_line:
+            source_span = line.find('span', class_='tag_s')
+            target_span = line.find('span', class_='tag_t')
+            if source_span and target_span:
+                examples.append(source_span.text)
+                examples.append(target_span.text)
+
+        examples_container = soup.find('div', class_='example_lines inexact')
+
+        if examples_container is not None:
+            if lan == "fr":
+                source_link = examples_container.find_all('a', class_='dictLink', href=lambda h: h.startswith('/english-french'))
+                target_link = examples_container.find_all('a', class_='dictLink', href=lambda h: h.startswith('/french-english'))
+            else:
+                source_link = examples_container.find_all('a', class_='dictLink', href=lambda h: h.startswith('/french-english'))
+                target_link = examples_container.find_all('a', class_='dictLink', href=lambda h: h.startswith('/english-french'))
+
+
+            for source, target in zip(source_link, target_link):
+                examples.append(source.text)
+                examples.append(target.text)
+
+        return words, examples, lan
+
+    def output(self):
+        words, examples, lan = self.fetcher()
+
         if lan == "fr":
-            source_link = examples_container.find_all('a', class_='dictLink', href=lambda h: h.startswith('/english-french'))
-            target_link = examples_container.find_all('a', class_='dictLink', href=lambda h: h.startswith('/french-english'))
+            print("\nFrench Translations:")
         else:
-            source_link = examples_container.find_all('a', class_='dictLink', href=lambda h: h.startswith('/french-english'))
-            target_link = examples_container.find_all('a', class_='dictLink', href=lambda h: h.startswith('/english-french'))
+            print("\nEnglish Translations:")
 
+        for word in words:
+            print(word)
 
-        for source, target in zip(source_link, target_link):
-            examples.append(source.text)
-            examples.append(target.text)
+        if lan == "fr":
+            print("\nFrench Examples:")
+        else:
+            print("\nEnglish Examples:")
 
-        return words, examples
-
-
-
-
+        for i in range(0, len(examples), 2):
+            pair = examples[i:i + 2]
+            print(*pair, sep='\n')
+            if i + 2 < len(examples):
+                print()
